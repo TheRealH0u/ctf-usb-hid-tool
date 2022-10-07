aafed8e3eecc4c50887f5174ee36217d42a999f6
Hou <aplazl55@gmail.com>
2022-10-07 21:05:48 +0200

    Update usbhidscript.py

--- usbhidscript.py
+++ usbhidscript.py
@@ -1,214 +1,185 @@
 import sys
 import subprocess
 
-lcasekey = {}
-ucasekey = {}
+KEY_CODES = {
+    4: ['a', 'A'],
+    5: ['b', 'B'],
+    6: ['c', 'C'],
+    7: ['d', 'D'],
+    8: ['e', 'E'],
+    9: ['f', 'F'],
+    10: ['g', 'G'],
+    11: ['h', 'H'],
+    12: ['i', 'I'],
+    13: ['j', 'J'],
+    14: ['k', 'K'],
+    15: ['l', 'L'],
+    16: ['m', 'M'],
+    17: ['n', 'N'],
+    18: ['o', 'O'],
+    19: ['p', 'P'],
+    20: ['q', 'Q'],
+    21: ['r', 'R'],
+    22: ['s', 'S'],
+    23: ['t', 'T'],
+    24: ['u', 'U'],
+    25: ['v', 'V'],
+    26: ['w', 'W'],
+    27: ['x', 'X'],
+    28: ['y', 'Y'],
+    29: ['z', 'Z'],
+    30: ['1', '!'],
+    31: ['2', '@'],
+    32: ['3', '#'],
+    33: ['4', '$'],
+    34: ['5', '%'],
+    35: ['6', '^'],
+    36: ['7', '&'],
+    37: ['8', '*'],
+    38: ['9', '('],
+    39: ['0', ')'],
+    40: ['Enter', 'Enter'],
+    41: ['<esc>', '<esc>'],
+    42: ['backspace', 'backspace'],
+    43: ['tab', 'tab'],
+    44: ['space', 'space'],
+    45: ['-', '_'],
+    46: ['=', '+'],
+    47: ['[', '{'],
+    48: [']', '}'],
+    49: ['\\', '|'],
+    50: [' ', ' '],
+    51: [';', ':'],
+    52: ["'", '"'],
+    53: ['`', '~'],
+    54: [',', '<'],
+    55: ['.', '>'],
+    56: ['/', '?'],
+    57: ['CapsLock', 'CapsLock'],
+    74: ['home', 'home'],
+    76: ['del', 'del'],
+    77: ['end', 'end'],
+    79: ['Right', 'Right'],
+    80: ['Left', 'Left'],
+    81: ['Down', 'Down'],
+    82: ['Up', 'Up'],
+    84: ['/', '/'],
+    85: ['*', '*'],
+    86: ['-', '-'],
+    87: ['+', '+'],
+    88: ['Enter', 'Enter'],
+    89: ['1', '1'],
+    90: ['2', '2'],
+    91: ['3', '3'],
+    92: ['4', '4'],
+    93: ['5', '5'],
+    94: ['6', '6'],
+    95: ['7', '7'],
+    96: ['8', '8'],
+    97: ['9', '9'],
+    98: ['0', '0'],
+    99: ['.', '.'],
+}
 
-lcasekey[4] = "a"
-ucasekey[4] = "A"
-lcasekey[5] = "b"
-ucasekey[5] = "B"
-lcasekey[6] = "c"
-ucasekey[6] = "C"
-lcasekey[7] = "d"
-ucasekey[7] = "D"
-lcasekey[8] = "e"
-ucasekey[8] = "E"
-lcasekey[9] = "f"
-ucasekey[9] = "F"
-lcasekey[10] = "g"
-ucasekey[10] = "G"
-lcasekey[11] = "h"
-ucasekey[11] = "H"
-lcasekey[12] = "i"
-ucasekey[12] = "I"
-lcasekey[13] = "j"
-ucasekey[13] = "J"
-lcasekey[14] = "k"
-ucasekey[14] = "K"
-lcasekey[15] = "l"
-ucasekey[15] = "L"
-lcasekey[16] = "m"
-ucasekey[16] = "M"
-lcasekey[17] = "n"
-ucasekey[17] = "N"
-lcasekey[18] = "o"
-ucasekey[18] = "O"
-lcasekey[19] = "p"
-ucasekey[19] = "P"
-lcasekey[20] = "q"
-ucasekey[20] = "Q"
-lcasekey[21] = "r"
-ucasekey[21] = "R"
-lcasekey[22] = "s"
-ucasekey[22] = "S"
-lcasekey[23] = "t"
-ucasekey[23] = "T"
-lcasekey[24] = "u"
-ucasekey[24] = "U"
-lcasekey[25] = "v"
-ucasekey[25] = "V"
-lcasekey[26] = "w"
-ucasekey[26] = "W"
-lcasekey[27] = "x"
-ucasekey[27] = "X"
-lcasekey[28] = "y"
-ucasekey[28] = "Y"
-lcasekey[29] = "z"
-ucasekey[29] = "Z"
-lcasekey[30] = "1"
-ucasekey[30] = "!"
-lcasekey[31] = "2"
-ucasekey[31] = "@"
-lcasekey[32] = "3"
-ucasekey[32] = "#"
-lcasekey[33] = "4"
-ucasekey[33] = "$"
-lcasekey[34] = "5"
-ucasekey[34] = "%"
-lcasekey[35] = "6"
-ucasekey[35] = "^"
-lcasekey[36] = "7"
-ucasekey[36] = "&"
-lcasekey[37] = "8"
-ucasekey[37] = "*"
-lcasekey[38] = "9"
-ucasekey[38] = "("
-lcasekey[39] = "0"
-ucasekey[39] = ")"
-lcasekey[40] = "Enter"
-ucasekey[40] = "Enter"
-lcasekey[41] = "<esc>"
-ucasekey[41] = "<esc>"
-lcasekey[42] = "backspace"
-ucasekey[42] = "backspace"
-lcasekey[43] = "tab"
-ucasekey[43] = "tab"
-lcasekey[44] = "space"
-ucasekey[44] = "space"
-lcasekey[45] = "-"
-ucasekey[45] = "_"
-lcasekey[46] = "="
-ucasekey[46] = "+"
-lcasekey[47] = "["
-ucasekey[47] = "{"
-lcasekey[48] = "]"
-ucasekey[48] = "}"
-lcasekey[49] = "\\"
-ucasekey[49] = "|"
-lcasekey[50] = " "
-ucasekey[50] = " "
-lcasekey[51] = ";"
-ucasekey[51] = ":"
-lcasekey[52] = "'"
-ucasekey[52] = "\""
-lcasekey[53] = "`"
-ucasekey[53] = "~"
-lcasekey[54] = ","
-ucasekey[54] = "<"
-lcasekey[55] = "."
-ucasekey[55] = ">"
-lcasekey[56] = "/"
-ucasekey[56] = "?"
-lcasekey[57] = "CapsLock"
-ucasekey[57] = "CapsLock"
+# Predefined variables
+# ------
+output = [""] # output
+holdingShift = False # Variable for KeyboardMove()
+position_x_shift = 0 # Position of cursor while highlighting
+position_x = 0 # Position of cursor
+position_y = 0 # Position of line
+copyText = ""
+# ------
 
-ucasekey[74] = "home"
-lcasekey[74] = "home"
+# Keyboard functions
+def KeyboardMove(data, shift=False):
+	global holdingShift
+	global position_x
+	global position_y
+	global position_x_shift
 
-ucasekey[76] = "del"
-lcasekey[76] = "del"
-
-ucasekey[77] = "end"
-lcasekey[77] = "end"
-
-lcasekey[79] = "Right"
-ucasekey[79] = "Right"
-lcasekey[80] = "Left"
-ucasekey[80] = "Left"
-lcasekey[81] = "Down"
-ucasekey[81] = "Down"
-lcasekey[82] = "Up"
-ucasekey[82] = "Up"
-
-lcasekey[84] = "/"
-ucasekey[84] = "/"
-lcasekey[85] = "*"
-ucasekey[85] = "*"
-lcasekey[86] = "-"
-ucasekey[86] = "-"
-lcasekey[87] = "+"
-ucasekey[87] = "+"
-lcasekey[88] = "Enter"
-ucasekey[88] = "Enter"
-lcasekey[89] = "1"
-ucasekey[89] = "1"
-lcasekey[90] = "2"
-ucasekey[90] = "2"
-lcasekey[91] = "3"
-ucasekey[91] = "3"
-lcasekey[92] = "4"
-ucasekey[92] = "4"
-lcasekey[93] = "5"
-ucasekey[93] = "5"
-lcasekey[94] = "6"
-ucasekey[94] = "6"
-lcasekey[95] = "7"
-ucasekey[95] = "7"
-lcasekey[96] = "8"
-ucasekey[96] = "8"
-lcasekey[97] = "9"
-ucasekey[97] = "9"
-lcasekey[98] = "0"
-ucasekey[98] = "0"
-lcasekey[99] = "."
-ucasekey[99] = "."
-
-# Predefine variables that are gonna be used
-outString = [""]
-position = 0
-oSline = 0 #Short for outStringline
-
-def isEnter(data):
-	if ucasekey[data] == "Enter":
-		return True
-	else:
-		return False
-
-def USBHIDFunction(data):
-	global outString
-	global oSline
-	global position
-	if data == "Enter" or data == "tab":
-		outString.append("")
-		oSline += 1
-	elif data == "space":
-		outString[oSline] += " "
-	elif data == "Left":
-		position -= 1
+	if data == "Left":
+		if shift and not holdingShift:
+			position_x_shift = position_x # If were holding shift we take the current position so we know how many character we highlighted.
+			holdingShift = True # If were still holding shift, we musnt overwrite the previous position shift, thats why we make a variable hodling shift to account for that
+			position_x -= 1
+		elif not shift and holdingShift:
+			holdingShift = False # If were not holding shift were not holding shift
+			if position_x > position_x_shift: # If we highlight to the right and the press left we go back to the previous position on position_x_shift
+				position_x = position_x_shift 
+			# If we press left when we highlighted to the left we just leave the position_x as it is
+		else:
+			position_x -= 1
 	elif data == "Right":
-		position += 1
+		if shift and not holdingShift:
+			position_x_shift = position_x
+			holdingShift = True
+			position_x += 1
+		elif not shift and holdingShift:
+			holdingShift = False
+			if position_x < position_x_shift: # Same here. If we highlight to the left and press right we go back to the highlighting position
+				position_x = position_x_shift
+			# On the other hand we go to the position that is position_x
+		else:
+			position_x += 1
 	elif data == "Up":
-		oSline -= 1
+		""" TODO: 
+		Support for multi line highlighting with up and down
+		"""
+		
+		position_y -= 1
 	elif data == "Down":
-		oSline += 1
+		position_y += 1
+def KeyboardCopyPaste(data):
+	global position_x
+	global position_y
+	global position_x_shift
+	global copyText
+	global output
+
+	if data == "c":
+		copyText = output[position_y]
+		if position_x > position_x_shift:
+			copyText = copyText[position_x_shift:position_x]
+		else:
+			copyText = copyText[position_x:position_x_shift]
+	elif data == "v":
+		txtLeft, txtRight = output[position_y][:position_x],output[position_y][position_x:]
+		txtLeft += copyText
+		output[position_y] = txtLeft + txtRight
+		position_x += len(copyText)
+def KeyboardPrint(data):
+	global output
+	global position_y
+	global position_x 
+		
+	if data == "Enter" or data == "tab":
+		output.append("")
+		position_y += 1
+		position_x = 0
+	elif data == "space":
+		txtLeft, txtRight = output[position_y][:position_x],output[position_y][position_x:]
+		txtLeft += " "
+		output[position_y] = txtLeft + txtRight
+		position_x += 1
 	elif data == "del":
-		txtLeft, txtRight = outString[oSline][:len(outString[oSline])+position],outString[oSline][len(outString[oSline])+position:]
+		txtLeft, txtRight = output[position_y][:position_x],output[position_y][position_x:]
 		txtRight = txtRight[1:]
-		outString[oSline] = txtLeft + txtRight
-		position += 1
+		output[position_y] = txtLeft + txtRight
 	elif data == "backspace":
-		txtLeft, txtRight = outString[oSline][:len(outString[oSline])+position],outString[oSline][len(outString[oSline])+position:]
+		txtLeft, txtRight = output[position_y][:position_x],output[position_y][position_x:]
 		txtLeft = txtLeft[:-1]
-		outString[oSline] = txtLeft + txtRight
+		output[position_y] = txtLeft + txtRight
+		position_x -= 1
 	elif data == "home":
-		position = -len(outString[oSline])
+		position_x = 0
 	elif data == "end":
-		position = 0
+		position_x = len(output[position_y])
 	else:
-		txtLeft, txtRight = outString[oSline][:len(outString[oSline])+position],outString[oSline][len(outString[oSline])+position:]
+		txtLeft, txtRight = output[position_y][:position_x],output[position_y][position_x:]
 		txtLeft += data
-		outString[oSline] = txtLeft + txtRight
+		output[position_y] = txtLeft + txtRight
+		position_x += 1
 		
 
 
@@ -233,18 +204,27 @@ if (len(sys.argv)) == 2:
 	for line in extractedKeys:
 		line = line.split(":")
 		hexKey = int(line[2], 16)
-		if hexKey in ucasekey:
+		if hexKey in KEY_CODES:
 			if line[3] == "00":
-				#LCTRL and RCTRL
+
+				# LCTRL and RCTRL modifier
 				if line[0] == "01" or line[0] == "10":
-					# TO DO
-					print("CTRL+"+lcasekey[hexKey])
-				#LSHIFT, RSHIFT and RALT
+					KeyboardCopyPaste(KEY_CODES[hexKey][0])
+
+				#LSHIFT, RSHIFT and RALT modifiers
 				elif line[0] == "02" or line[0] == "20" or line[0] == "40":
-					USBHIDFunction(ucasekey[hexKey])
+					if KEY_CODES[hexKey][0] in ["Up","Down","Left","Right"]:
+						KeyboardMove(KEY_CODES[hexKey][0], True)
+					else:
+						KeyboardPrint(KEY_CODES[hexKey][1])
 				else:
-					USBHIDFunction(lcasekey[hexKey])
-	for s in outString:
+					if KEY_CODES[hexKey][0] in ["Up","Down","Left","Right"]:
+						KeyboardMove(KEY_CODES[hexKey][0])
+					else:
+						KeyboardPrint(KEY_CODES[hexKey][0])
+
+	# Print output line by line
+	for s in output:
 		print(s)
 else:
 	print("[?] python3 usb-hid-script.py [filename/.pcapng/.pcap]")
